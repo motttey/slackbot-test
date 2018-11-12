a86624bb4a641fa32c6fb7d907a1494f96c254de
motttey <motitago@gmail.com>
2018-11-12 19:08:52 +0900

    show months duties

--- plugins/my_mention.py
+++ plugins/my_mention.py
@@ -5,19 +5,33 @@ from slackbot.bot import listen_to
 from slackbot.bot import default_reply
 from datetime import datetime
 from datetime import date
+from datetime import timedelta
+
 from math import ceil
 
 duty_dic = { 0:'B4', 1:'M1', 2:'M2', 3:'Doctor'}
-@respond_to('gomiday')
-def mention_func(message):
-    message.reply('私にメンションと言ってどうするのだ')
+
+def get_start_end_dates(year, week):
+    d = date(year,1,1)
+    if(d.weekday()<= 3):
+        d = d - timedelta(d.weekday())
+    else:
+        d = d + timedelta(7-d.weekday())
+    dlt = timedelta(days = (week-1)*7)
+    first_day = str(d + dlt);
+    end_day = str(d + dlt + timedelta(days=6));
+    return '( from: ' + first_day + ' to: ' + end_day + ' )'
+
+@default_reply()
+def default_func(message):
+    message.reply('ゴミ')
 
 @respond_to('今週のゴミ')
 def mention_func(message):
     today = datetime.now()
     today_calender = today.isocalendar()
     duty_index = today_calender[2] % 4
-    message.reply('今週のゴミ担当は' + duty_dic[duty_index] + 'です')
+    message.reply('今週のゴミ担当は ' + duty_dic[duty_index] + ' です')
 
 @respond_to('今月のゴミ')
 def mention_func(message):
@@ -27,8 +41,9 @@ def mention_func(message):
 
     today = datetime.now()
     today_calender = today.isocalendar()
-    first_day_calender = today.isocalendar()
+    first_day_calender = first_day.isocalendar()
 
-    for w in range(week_number):
-        duty_index = (w + today_calender[2] - first_day_calender[2]) % 4
-        message.reply(str(w)+ '週目のゴミ担当は' +duty_dic[duty_index]+ 'です')
+    for w in range(1, week_number + 1):
+        current_week = w + first_day_calender[1]
+        duty_index = (current_week - 1) % 4
+        message.reply(str(w)+ '週目' + get_start_end_dates(today_calender[0], current_week)+ 'のゴミ担当は' +duty_dic[duty_index]+ 'です')
