63cbf371280e7ea48ac79b133a4ea03129416443
motttey <motitago@gmail.com>
2018-11-17 16:25:01 +0900

    fix listen, week caluculation

--- plugins/my_mention.py
+++ plugins/my_mention.py
@@ -8,6 +8,7 @@ from datetime import date
 from datetime import timedelta
 
 from math import ceil
+import calendar
 
 duty_dic = { 0:'B4', 1:'M1', 2:'M2', 3:'Doctor'}
 
@@ -22,28 +23,29 @@ def get_start_end_dates(year, week):
     end_day = str(d + dlt + timedelta(days=6));
     return '( from: ' + first_day + ' to: ' + end_day + ' )'
 
-@default_reply()
-def default_func(message):
+@respond_to('hello')
+def mention_func(message):
     message.reply('ゴミ')
 
 @listen_to('今週のゴミ')
-def mention_func(message):
+def listen_func(message):
     today = datetime.now()
     today_calender = today.isocalendar()
     duty_index = today_calender[2] % 4
-    message.reply('今週のゴミ担当は ' + duty_dic[duty_index] + ' です')
+    message.send('今週のゴミ担当は ' + duty_dic[duty_index] + ' です')
 
 @listen_to('今月のゴミ')
-def mention_func(message):
-    day_of_month = datetime.now().day
-    week_number = (day_of_month - 1) % 7
+def listen_func(message):
     first_day = date.today().replace(day=1)
 
-    today = datetime.now()
-    today_calender = today.isocalendar()
+    today = datetime.today()
+    today_calender = datetime.now().isocalendar()
     first_day_calender = first_day.isocalendar()
+
+    day_of_month = calendar.monthrange(today.year, today.month)
+    week_number = int((day_of_month[1]) / 7)
 
     for w in range(1, week_number + 1):
         current_week = w + first_day_calender[1]
         duty_index = (current_week - 1) % 4
-        message.reply(str(w)+ ' 週目 ' + get_start_end_dates(today_calender[0], current_week)+ 'のゴミ担当は' +duty_dic[duty_index]+ 'です')
+        message.send(str(w)+ ' 週目 ' + get_start_end_dates(today_calender[0], current_week)+ 'のゴミ担当は' +duty_dic[duty_index]+ 'です')
